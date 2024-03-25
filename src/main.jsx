df20b2d5bafadca4bc15940fb715346728189cfe
toriqul07 <torikul170074@gmail.com>
2024-03-25 23:24:43 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Root from './components/Root/Root';
+import Home from './components/Home/Home';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs';
+import ErrorPage from './components/ErrorPage/ErrorPage';
+import JObDetails from './components/JobDetails/JObDetails';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }, 
+      {
+        path: '/applied', 
+        element: <AppliedJobs></AppliedJobs>,
+        loader: () => fetch('/jobs.json') // warning: only load the data you need. do not load all the data
+      },
+      {
+        path: '/job/:id',
+        element:<JObDetails></JObDetails>, 
+        loader: () => fetch('/jobs.json') // do not load all data. load only what you need
+      }
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
